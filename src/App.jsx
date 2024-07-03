939d6cd3c7ae4a7e55474beaf927bf0dc7dc3b90
Dilip Kumar <suthardilip189@gmail.com>
2024-07-03 17:05:05 +0530

    upload commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,16 @@ import { TwoWheeler } from '@components/Pages/TwoWheeler';
 import { FourDetails, ThreeDetails,TwoDetails} from '@Vehicle';
 import Dashboard from './Admin/Pages/Dashboard';
 import Profile from './Admin/Pages/Profile';
+import ABrand from '@Admin/Pages/ABrand';
+import ADealer from '@Admin/Pages/ADealer';
+import AEVSpares from '@Admin/Pages/AEVSpares';
+import AEVStartups from '@Admin/Pages/AEVStartups';
+import AFourWheeler from '@Admin/Pages/AFourWheeler';
+import ANews from '@Admin/Pages/ANews';
+import AThreeWheeler from '@Admin/Pages/AThreeWheeler';
+import ATwoWheeler from '@Admin/Pages/ATwoWheeler';
+
+
 
 
 function App() {
@@ -32,13 +42,22 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          {/* Admin  */}
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="//dashboard/profile" element={<Profile />} />
+          <Route path="/dashboard/profile" element={<Profile />} />
+          <Route path="/dashboard/brand" element={<ABrand />} />
+          <Route path="/dashboard/dealer" element={<ADealer />} />
+          <Route path="/dashboard/evspares" element={<AEVSpares />} />
+          <Route path="/dashboard/evstartups" element={<AEVStartups />} />
+          <Route path="/dashboard/fourwheeler" element={<AFourWheeler />} />
+          <Route path="/dashboard/news" element={<ANews />} />
+          <Route path="/dashboard/threewheeler" element={<AThreeWheeler />} />
+          <Route path="/dashboard/twowheeler" element={<ATwoWheeler />} />
+
           <Route path="/" element={<Home />} />
           <Route path="/error" element={<Error404 />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/twoWheeler" element={<TwoWheeler />} />
           <Route path="/twodetails" element={<TwoDetails />} />
           <Route path="/threedetails" element={<ThreeDetails />} />
           <Route path="/fourdetails" element={<FourDetails />} />
