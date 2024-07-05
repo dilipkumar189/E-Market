49cea2f8959d46bc03b0a4ccc5b4324642b9eba3
Dilip Kumar <suthardilip189@gmail.com>
2024-07-05 10:42:15 +0530

    edit forms

--- Client/src/Admin/Pages/ANews/News_Form.jsx
+++ Client/src/Admin/Pages/ANews/News_Form.jsx
@@ -8,11 +8,11 @@ export const News_Form = () => {
         <Header />
         <Sidebar />
         <div className="px-4 mt-3 sm:ml-64">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 border-[1px] rounded-lg shadow-sm ">
-            <div className="grid grid-cols-9">
-            <div>News FORM</div>
-            </div>  
-        </div>
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 border-[1px] rounded-lg shadow-sm ">
+              <div className="grid grid-cols-9">
+                <div>News FORM</div>
+              </div>  
+          </div>
         </div>
     </div>
   )
