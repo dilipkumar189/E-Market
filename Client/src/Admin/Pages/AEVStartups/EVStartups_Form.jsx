e88e8115be902620d7541f73a83d52b9a18e5b3e
Dilip Kumar <suthardilip189@gmail.com>
2024-07-05 11:37:09 +0530

    update design

--- Client/src/Admin/Pages/AEVStartups/EVStartups_Form.jsx
+++ Client/src/Admin/Pages/AEVStartups/EVStartups_Form.jsx
@@ -9,9 +9,11 @@ export const EVStartups_Form = () => {
         <Sidebar />
         <div className="px-4 mt-3 sm:ml-64">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 border-[1px] rounded-lg shadow-sm ">
-              <div className="grid grid-cols-9">
-                <div>Dealer FORM</div>
-              </div>  
+            <div className="grid grid-cols-9 border-b-[1px]">
+              <h1 className="text-gray-600 font-bold text-lg mb-3 col-span-7 md:col-span-8">
+                Add EV Startups Details
+              </h1>
+            </div>  
           </div>
         </div>
     </div>
