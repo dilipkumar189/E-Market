f2e9420a0db5e31387baab60bb82df0cd40f7c76
Dilip Kumar <suthardilip189@gmail.com>
2024-07-09 17:00:11 +0530

    add forms

--- Client/src/Admin/Pages/Profile/index.jsx
+++ Client/src/Admin/Pages/Profile/index.jsx
@@ -5,7 +5,7 @@ import Header from '@Admin/Layouts/Header';
 
 const inputCss = "block py-1 px-2 w-full text-sm text-gray-900 bg-transparent border-[1px] border-gray-300 rounded-[4px] appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 
-const labelCss = "peer-focus:font-medium px-2 text-[13px] absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+const labelCss = "peer-focus:font-medium px-2 text-[13px] absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-70 top-1 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
 
 
 export default function Profile() {
