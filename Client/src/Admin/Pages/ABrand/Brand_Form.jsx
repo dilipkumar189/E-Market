68e6bfc2381c488bef1eccc76ae00d553119cc08
Dilip Kumar <suthardilip189@gmail.com>
2024-07-09 15:39:00 +0530

    add forms

--- Client/src/Admin/Pages/ABrand/Brand_Form.jsx
+++ Client/src/Admin/Pages/ABrand/Brand_Form.jsx
@@ -37,7 +37,7 @@ export const Brand_Form = () => {
                           type="text"
                           placeholder="janesmith"
                           autoComplete="username"
-                          className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                          className="block flex-1 border-2 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                         />
                       </div>
                     </div>
@@ -128,13 +128,13 @@ export const Brand_Form = () => {
                         name="last-name"
                         type="text"
                         autoComplete="family-name"
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="block w-full border-[1px] rounded-md py-1 border-slate-600 "
                       />
                     </div>
                   </div>
 
                   <div className="sm:col-span-4">
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-700">
                       Email address
                     </label>
                     <div className="mt-2">
--- Client/src/Admin/Pages/Profile/index.jsx
+++ Client/src/Admin/Pages/Profile/index.jsx
@@ -18,7 +18,7 @@ export default function Profile() {
                   type="email"
                   name="floating_email"
                   id="floating_email"
-                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-b-[1px] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-[1px] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
                 />
