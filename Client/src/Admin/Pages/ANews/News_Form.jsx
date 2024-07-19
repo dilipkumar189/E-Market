c99b8b40fa776b1dae90b993e23f0391cb5924a9
Dilip Kumar <suthardilip189@gmail.com>
2024-07-19 16:50:06 +0530

    update news

--- Client/src/Admin/Pages/ANews/News_Form.jsx
+++ Client/src/Admin/Pages/ANews/News_Form.jsx
@@ -15,7 +15,7 @@ export const News_Form = () => {
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 border-[1px] rounded-lg shadow-sm ">
         <div className="grid grid-cols-9 border-b-[1px]">
           <h1 className="text-gray-600 font-bold text-lg mb-3 col-span-7 md:col-span-8">
-            Add Brand Details
+            Add News Details
           </h1>
         </div>
         <form className="mt-4">
