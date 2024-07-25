6d59d9d01fb46d3d1867e2605aaabd68b20827ee
Dilip Kumar <suthardilip189@gmail.com>
2024-07-25 12:12:01 +0530

    Update E-Market

--- Client/src/User/components/Pages/WheelerDetails/FourDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function FourDetails() {
-  return (
-    <>
-        <h1>Four Wheeler Details</h1>
-    </>      
-  )
-}
--- Client/src/User/components/Pages/WheelerDetails/ThreeDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ThreeDetails() {
-  return (
-    <>
-        <h1>Three Wheeler Details</h1>
-    </>
-  )
-}
