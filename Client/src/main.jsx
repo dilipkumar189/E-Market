202e9597692688b5d25aafc2404f16f2ddd173d4
Dilip Kumar <127296438+dilipkumar189@users.noreply.github.com>
2025-02-16 09:14:06 -0800

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 // import "owl.carousel/dist/assets/owl.carousel.css";
 // import "owl.carousel/dist/assets/owl.theme.default.css";
-// import "jquery"; // Import jQuery
+// import "jquery"; //  Import jQuery
 // import "owl.carousel";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
